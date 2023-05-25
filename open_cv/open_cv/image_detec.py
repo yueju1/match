f1129f74b27afcd88dc5258c39d0797cf57ec288
Yue Ju <juyue0835@gmail.com>
2023-05-25 22:58:46 +0200

    code

--- open_cv/open_cv/image_detec.py
+++ open_cv/open_cv/image_detec.py
@@ -24,7 +24,6 @@ class ImageSubscriber(Node):
 
         current_frame = cv2.imread(
         '/home/yueju/下载/Greifer_Unterseitenkamera.bmp')
-
         
         #先看看canny,threshold值,对检测到的个数的影响.   再  搞一下转动之后圆有偏差的问题
     # def listener_callback(self, data):
@@ -50,19 +49,19 @@ class ImageSubscriber(Node):
         
 
         kernel = np.ones((5,5),np.uint8)
-        
+        cv2.find
         kernel2 = np.ones((10,10),np.uint8)
         pengzhang = cv2.dilate(gray,kernel2)
         fushi =cv2.erode(pengzhang,kernel)
         kai = cv2.morphologyEx(gray,cv2.MORPH_CLOSE,kernel2,iterations=1)
-
+       # cv2.sobel  Gradient
         part = current_frame[680:1550, 750:1750]  # to be modified
         asd = cv2.resize(part, (0, 0), fx=10, fy=10)
         edges = cv2.Canny(gray2, threshold1=30, threshold2=60)
         dst2 = cv2.pyrDown(current_frame)  #轮廓加粗!!!
         canny = cv2.Canny(gray2, 10, 800)
         p2 = cv2.dilate(canny,kernel)
-
+        
         ret, thresh = cv2.threshold(canny, 140, 220, cv2.THRESH_BINARY)
         #na ge fangfa geng zhunque
         contours, hierarchy = cv2.findContours(
@@ -72,42 +71,42 @@ class ImageSubscriber(Node):
     #   filter?
     # double houghcircles to detect two times
         #找轮廓的点，拟合圆
-    #     circles = cv2.HoughCircles(  # kleiner keris in 1.py
-    #     gray, cv2.HOUGH_GRADIENT, 1, 50, param1=30, param2=150, minRadius=5, maxRadius=150)
-    #     print(circles)
-    # # hough param und cany param
-    # # try: ? if there is no circle, output typeerror
-    #     for circle in circles[0]:
+        circles = cv2.HoughCircles(  # kleiner keris in 1.py
+        gray, cv2.HOUGH_GRADIENT, 1, 50, param1=30, param2=150, minRadius=5, maxRadius=150)
+        print(circles)
+    # hough param und cany param
+    # try: ? if there is no circle, output typeerror
+        for circle in circles[0]:
 
-    #         print(circle[2])
+            print(circle[2])
 
-    #         x = int(circle[0])
-    #         y = int(circle[1])
+            x = int(circle[0])
+            y = int(circle[1])
 
-    #         r = int(circle[2])
+            r = int(circle[2])
 
             
-    #         col = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-    #         cv2.circle(col, (x, y), r, (0, 0, 255), 3)
-    #         cv2.circle(col, (x, y), 5, (0, 0, 255), -1)
-    #         print(x, y, r)
+            col = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+            cv2.circle(col, (x, y), r, (0, 0, 255), 3)
+            cv2.circle(col, (x, y), 5, (0, 0, 255), -1)
+            print(x, y, r)
 
     # ru guo bianyuan jiance li you zhixian  then  loesch
     # ru guo yuan de shuliang > 2 then ...
     # center_points = []
     # # print(hierarchy[0])
 
-        for contour in contours:
-            moments = cv2.moments(contour)
-            if moments['m00'] != 0:
-        # ZeroDivisionError: float division by zero
-                cX = int(moments["m10"] / moments["m00"])
-                cY = int(moments["m01"] / moments["m00"])  # ausschneiden
-        # center_points.append((cX, cY))
-                print(cX, cY)     
-                col = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
-                # cv2.drawContours(col, contours, -1, (0, 0, 255), 2)
-                cv2.circle(col, (cX, cY), 5, (0, 0, 255), -1)
+        # for contour in contours:
+        #     moments = cv2.moments(contour)
+        #     # if moments['m00'] != 0:    # or try:...?
+        # # ZeroDivisionError: float division by zero
+        #     cX = int(moments["m10"] / moments["m00"])
+        #     cY = int(moments["m01"] / moments["m00"])  # ausschneiden
+        # # center_points.append((cX, cY))
+        #     print(cX, cY)     
+        #     col = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
+        #     cv2.drawContours(col, contours, -1, (0, 0, 255), 2)
+        #     cv2.circle(col, (cX, cY), 5, (0, 0, 255), -1)
         
         # cv2.circle(current_frame, (1999, 1999), 4, (0, 0, 255), -1)
 
@@ -119,16 +118,18 @@ class ImageSubscriber(Node):
     # then first - -> track the Trajectory:
                                     # alle miitelpunkte sammeln und den mittelpunkt berechnen
 
-            cv2.namedWindow('camera', 0)
-            cv2.resizeWindow("camera", 1000, 1000)
+        cv2.namedWindow('camera', 0)
+        cv2.resizeWindow("camera", 1000, 1000)
     # cv2.namedWindow('asd', 0)
     # cv2.resizeWindow("asd", 1000, 1000)
     # Display image
 
-            cv2.imshow("camera", col)
-    # cv2.imshow('asd', part)
+        cv2.imshow("camera", col)
+        cv2.namedWindow('asd', 0)
+        cv2.resizeWindow("asd", 1000, 1000)
+        cv2.imshow('asd', current_frame)
     # cv2.imshow('asd', casd)
-            cv2.waitKey()
+        cv2.waitKey()
 
 
 def main(args=None):
